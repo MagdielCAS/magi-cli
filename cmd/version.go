a384d0fe4da7bba8ae5621464c758edb7c3ea4ca
Magdiel Campelo <magdiel.camp@gmail.com>
2025-09-27 01:05:40 +0100

    feat: :sparkles: adds version command to CLI
    Introduces a new command to display the CLI version,
    build date, and commit hash.
    
    This provides users with essential information about the
    CLI build they are using.

---
+++ cmd/version.go
@@ -0,0 +1,74 @@
+/**
+ * Copyright © 2025 Magdiel Campelo <github.com/MagdielCAS/magi-cli>
+ * This file is part of the magi-cli
+**/
+package cmd
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/pterm/pterm"
+	"github.com/spf13/cobra"
+)
+
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Shows the version of magi",
+	Long: `Shows the current version of magi, build date, and commit hash.
+
+Usage:
+  magi version
+
+Examples:
+  # Default behavior
+  magi version
+
+  # Show version in JSON format
+  magi version --json
+
+Run 'magi version --help' for more information on a specific command.`,
+	Run: runVersion,
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+}
+
+func runVersion(cmd *cobra.Command, args []string) {
+	version, _ := cmd.Flags().GetBool("json")
+	if version {
+		printVersionJSON()
+	} else {
+		printVersion()
+	}
+}
+
+func printVersion() {
+	pterm.DefaultSection.Printf("PETE CLI Version: %s", version)
+
+	// Create a table for detailed version info
+	tableData := pterm.TableData{
+		{"Version", version},
+		{"Git Commit", commit},
+		{"Build Date", date},
+		{"Go Version", runtime.Version()},
+		{"OS/Arch", fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH)},
+	}
+
+	pterm.DefaultTable.WithHasHeader().WithData(tableData).Render()
+}
+
+func printVersionJSON() {
+	msg := fmt.Sprintf("PETE CLI Version: %s", version)
+	data := map[string]any{
+		"version":   version,
+		"commit":    commit,
+		"buildDate": date,
+		"goVersion": runtime.Version(),
+		"osArch":    fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
+	}
+	logger := pterm.DefaultLogger.WithLevel(pterm.LogLevelInfo).WithFormatter(pterm.LogFormatterJSON)
+
+	logger.Info(msg, logger.ArgsFromMap(data))
+}
