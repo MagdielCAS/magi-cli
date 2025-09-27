6f368fb3d9dbf3df8598c3f4f873a426982480f9
Magdiel Campelo <magdiel.camp@gmail.com>
2025-09-27 02:01:26 +0100

    tests: add tests for version cmd
    This change involves:
    - Updating references in version-related commands and tests.
    - Modifying the goreleaser configuration file.

---
+++ cmd/version_test.go
@@ -0,0 +1,37 @@
+/**
+ * Copyright © 2025 Magdiel Campelo <github.com/MagdielCAS/magi-cli>
+ * This file is part of the magi-cli
+**/
+package cmd
+
+import (
+	"bytes"
+	"os"
+	"strings"
+	"testing"
+
+	"github.com/pterm/pterm"
+)
+
+func TestVersionCmd(t *testing.T) {
+	rootCmd := GetRootCmd()
+	buf := new(bytes.Buffer)
+
+	// Redirect pterm output to the buffer
+	pterm.SetDefaultOutput(buf)
+	// Restore the original output at the end of the test
+	defer pterm.SetDefaultOutput(os.Stdout)
+
+	rootCmd.SetOut(buf)
+	rootCmd.SetErr(buf)
+	rootCmd.SetArgs([]string{"version"})
+
+	if err := rootCmd.Execute(); err != nil {
+		t.Fatalf("error executing version command: %v", err)
+	}
+
+	output := buf.String()
+	if !strings.Contains(output, "magi CLI Version: dev") {
+		t.Errorf("expected output to contain 'magi CLI Version: dev', but got '%s'", output)
+	}
+}
